7d3fcdda2c7c867a56e2b2c81566e7763ddab781
FaviWill <favisk8_@hotmail.com>
2019-01-05 04:16:29 -0200

    Ajuste conversor de moeda
    A chave tem uma limitação (plano free) que somente permite converter moeda de Euros (EUR) para outras moedas, então coloquei como padrão selecionado EUR no primeiro campo de seleção.

--- conversor-de-moedas/src/app/conversor/services/conversor.service.ts
+++ conversor-de-moedas/src/app/conversor/services/conversor.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
-//import { Http } from '@angular/http';
+// Importamos o HttpClient ao invés do Http para o Angular 6
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { 
-	Conversao,
-	ConversaoResponse 
+  Conversao,
+  ConversaoResponse 
 } from '../models';
 
 @Injectable()
 export class ConversorService {
-
+  // Nova url do fixer.io, que adiciona o parâmetro access_key, que é a chave de autenticação 
   //private readonly BASE_URL = "http://api.fixer.io/latest";
-  private readonly BASE_URL = "http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3";
-
+  private readonly BASE_URL = "http://data.fixer.io/api/latest?access_key=1d73a6ad965772f066d0c3f414d7ba7f&format=1";
   constructor(private http: HttpClient) {}
-
   /**
    * Realiza a chamada para a API de conversão de moedas.
    *
@@ -23,13 +21,14 @@ export class ConversorService {
    * @return Observable<ConversaoResponse>
    */
   converter(conversao: Conversao): Observable<any> {
-  	let params = `&base=${conversao.moedaDe}&symbols=${conversao.moedaPara}`;
-  	return this.http
+  // Na linha abaixo altere a '?' por '&'
+  let params = `&base=${conversao.moedaDe}&symbols=${conversao.moedaPara}`;
+  return this.http
       .get(this.BASE_URL + params);
+      // No Angular 6 as duas próximas linha não são mais necessárias
       //.map(response => response.json() as ConversaoResponse)
       //.catch(error => Observable.throw(error));
   }
-
   /**
    * Retorna a cotação para dado uma response.
    *
@@ -38,14 +37,12 @@ export class ConversorService {
    * @return number
    */
   cotacaoPara(conversaoResponse: ConversaoResponse, 
-		conversao: Conversao): number {
-  	if (conversaoResponse === undefined) {
-  		return 0;
-  	}
-
-  	return conversaoResponse.rates[conversao.moedaPara];
+ conversao: Conversao): number {
+  if (conversaoResponse === undefined) {
+  return 0;
   }
-
+  return conversaoResponse.rates[conversao.moedaPara];
+  }
   /**
    * Retorna a cotação de dado uma response.
    *
@@ -54,15 +51,13 @@ export class ConversorService {
    * @return string
    */
   cotacaoDe(conversaoResponse: ConversaoResponse, 
-		conversao: Conversao): string {
-  	if (conversaoResponse === undefined) {
-  		return '0';
-  	}
-
-  	return (1 / conversaoResponse.rates[conversao.moedaPara])
-  		.toFixed(4);
+ conversao: Conversao): string {
+  if (conversaoResponse === undefined) {
+  return '0';
   }
-
+  return (1 / conversaoResponse.rates[conversao.moedaPara])
+  .toFixed(4);
+  }
   /**
    * Retorna a data da cotação dado uma response.
    *
@@ -73,8 +68,6 @@ export class ConversorService {
     if (conversaoResponse === undefined) {
       return '';
     }
-
     return conversaoResponse.date;
   }
-
 }
